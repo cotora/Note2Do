c5a13165609b487577ce95d0b19033f530abd5df
cotora <cotora2121ia@gmail.com>
2025-05-03 23:47:00 +0900

    認識結果の画面を使いやすいように修正

--- front/detect_result_ui.py
+++ front/detect_result_ui.py
@@ -1,3 +1,5 @@
+import datetime
+
 import streamlit as st
 
 # ページ全体のレイアウトを中央寄せに
@@ -8,27 +10,51 @@ st.header("認識結果")
 
 # 認識結果表示エリア
 with st.container(border=True):
-
     # 日時表示
-    st.text_input("", value="2025/05/10 10:00〜11:00", disabled=True)
+    st.caption("日時")
+    date_col, start_time_col, time_separator_col, end_time_col = st.columns(
+        [2, 1, 0.2, 1]
+    )
 
-    # タスク名と編集・削除ボタンを横に配置
-    col1, col2, col3 = st.columns([3, 1, 1])
-
-    with col1:
-        st.write("タスク名")
-
-    with col2:
-        st.button(
-            "編集",
-            key="edit_button",
-            type="secondary",
-            help="このタスクを編集します",
+    with date_col:
+        st.date_input(
+            "日付",
+            value=datetime.date(2025, 5, 10),
+            label_visibility="collapsed",
         )
 
-    with col3:
+    with start_time_col:
+        st.time_input(
+            "開始時間",
+            value=datetime.time(10, 0),
+            label_visibility="collapsed",
+        )
+
+    with time_separator_col:
+        st.write("～")
+
+    with end_time_col:
+        st.time_input(
+            "終了時間",
+            value=datetime.time(11, 0),
+            label_visibility="collapsed",
+        )
+
+    # タスク名入力エリア
+    st.caption("タスク名")
+    task_name = st.text_input(
+        "タスク名", value="ミーティング", label_visibility="collapsed"
+    )
+
+    # 削除ボタン
+    delete_col1, delete_col2, delete_col3 = st.columns([1, 1, 1])
+    with delete_col2:
         st.button(
-            "削除", key="delete_button", type="secondary", help="このタスクを削除します"
+            "削除",
+            key="delete_button",
+            type="secondary",
+            help="このタスクを削除します",
+            use_container_width=True,
         )
 
 # スペースを追加
